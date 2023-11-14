4c2fb5fd155f01a554db632ea9b7933390e47e8c
Ben Ferenchak <ben.ferenchak@gmail.com>
2023-11-13 20:32:16 -0500

    Custom test nameing

--- src/test/java/com/adventofcode/year2019/AdventOfCodeTests.java
+++ src/test/java/com/adventofcode/year2019/AdventOfCodeTests.java
@@ -3,6 +3,7 @@ package com.adventofcode.year2019;
 
 import com.adventofcode.common.DailyAnswer;
 import com.adventofcode.year2019.day8.Layer;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,6 +14,7 @@ import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
+@DisplayName("2019")
 @SpringBootTest
 class AdventOfCodeTests {
 
--- src/test/java/com/adventofcode/year2022/AdventOfCodeTests.java
+++ src/test/java/com/adventofcode/year2022/AdventOfCodeTests.java
@@ -2,13 +2,14 @@ package com.adventofcode.year2022;
 
 
 import com.adventofcode.common.DailyAnswer;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
+@DisplayName("2022")
 @SpringBootTest
 class AdventOfCodeTests {
 
