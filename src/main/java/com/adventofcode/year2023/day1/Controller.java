336d83c0d637df8c9ca1bc8910096ef83f3ae7c1
Ben Ferenchak <ben.ferenchak@gmail.com>
2023-12-01 01:10:11 -0500

    2023 Day 1

--- src/main/java/com/adventofcode/year2023/day1/Controller.java
+++ src/main/java/com/adventofcode/year2023/day1/Controller.java
@@ -6,8 +6,6 @@ import com.adventofcode.common.SolutionController;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
 
 @Slf4j
 @Component("controller-2023-1")
@@ -19,12 +17,10 @@ public class Controller extends SolutionController {
 
     public DailyAnswer execute() {
 
-        List<Line> lines = puzzleInput.stream().map(s -> new Line(s, false)).toList();
-        answer.setPart1(lines.stream().map(Line::getValue).mapToInt(Integer::intValue).sum());
+        answer.setPart1(puzzleInput.stream().map(s -> new Line(s, false)).map(Line::getValue).mapToInt(Integer::intValue).sum());
         log.info("Part 1: {}", answer.getPart1());
 
-        lines = puzzleInput.stream().map(s -> new Line(s, true)).toList();
-        answer.setPart2(lines.stream().map(Line::getValue).mapToInt(Integer::intValue).sum());
+        answer.setPart2(puzzleInput.stream().map(s -> new Line(s, true)).map(Line::getValue).mapToInt(Integer::intValue).sum());
         log.info("Part 2: {}", answer.getPart2());
 
         return answer;
