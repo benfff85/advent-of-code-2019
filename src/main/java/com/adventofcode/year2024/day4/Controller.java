9a133aae266488cb452335a78947bf83f4c4832d
Ben Ferenchak <ben.ferenchak@gmail.com>
2024-12-11 22:56:23 +0100

    Cleanup

--- src/main/java/com/adventofcode/year2024/day4/Controller.java
+++ src/main/java/com/adventofcode/year2024/day4/Controller.java
@@ -3,7 +3,6 @@ package com.adventofcode.year2024.day4;
 import com.adventofcode.common.DailyAnswer;
 import com.adventofcode.common.InputHelper;
 import com.adventofcode.common.SolutionController;
-import com.adventofcode.common.grid.Direction;
 import com.adventofcode.common.grid.GridUtility;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -26,51 +25,29 @@ public class Controller extends SolutionController {
     public DailyAnswer execute() {
 
         Map<Point, GridElement> grid = constructGrid(puzzleInput, GridElement.class);
+        int count;
 
-        int count = 0;
-        List<Map.Entry<Point, GridElement>> fragment;
-        for(Point point : grid.keySet()) {
-            fragment = GridUtility.getAdjacentElements(grid, point, Direction.L, 4);
-            if(isXmas(fragment)) {count++;}
+        Map<Point, GridElement> pattern = GridUtility.constructGridWithoutSelectElements(List.of("XMAS"), GridElement.class, List.of(GridElement.SPACE));
+        count = countRotatedPatternMatches(grid, pattern);
 
-            fragment = GridUtility.getAdjacentElements(grid, point, Direction.U, 4);
-            if(isXmas(fragment)) {count++;}
+        pattern = GridUtility.constructGridWithoutSelectElements(List.of(
+                "X   ",
+                " M  ",
+                "  A ",
+                "   S"
+        ), GridElement.class, List.of(GridElement.SPACE));
+        count += countRotatedPatternMatches(grid, pattern);
 
-            fragment = GridUtility.getAdjacentElements(grid, point, Direction.R, 4);
-            if(isXmas(fragment)) {count++;}
-
-            fragment = GridUtility.getAdjacentElements(grid, point, Direction.D,4);
-            if(isXmas(fragment)) {count++;}
-
-            fragment = GridUtility.getAdjacentElements(grid, point, Direction.UL, 4);
-            if(isXmas(fragment)) {count++;}
-
-            fragment = GridUtility.getAdjacentElements(grid, point, Direction.UR, 4);
-            if(isXmas(fragment)) {count++;}
-
-            fragment = GridUtility.getAdjacentElements(grid, point, Direction.DL, 4);
-            if(isXmas(fragment)) {count++;}
-
-            fragment = GridUtility.getAdjacentElements(grid, point, Direction.DR, 4);
-            if(isXmas(fragment)) {count++;}
-
-        }
         answer.setPart1(count);
         log.info("Part 1: {}", answer.getPart1());
 
-        Map<Point, GridElement> pattern = GridUtility.constructGridWithoutSelectElements(List.of(
+
+        pattern = GridUtility.constructGridWithoutSelectElements(List.of(
                 "M S",
                 " A ",
                 "M S"
         ), GridElement.class, List.of(GridElement.SPACE));
-
-        count = IntStream.range(0, 4)
-                .map(i -> {
-                    int matches = GridUtility.getPatternMatches(grid, pattern).size();
-                    GridUtility.rotateGridClockwise(pattern);
-                    return matches;
-                })
-                .sum();
+        count = countRotatedPatternMatches(grid, pattern);
 
         answer.setPart2(count);
         log.info("Part 2: {}", answer.getPart2());
@@ -78,8 +55,14 @@ public class Controller extends SolutionController {
         return answer;
     }
 
-    private boolean isXmas(List<Map.Entry<Point, GridElement>> fragment) {
-        return fragment.stream().map(Map.Entry::getValue).toList().equals(List.of(GridElement.X, GridElement.M, GridElement.A, GridElement.S));
+    private int countRotatedPatternMatches(Map<Point, GridElement> grid, Map<Point, GridElement> pattern) {
+        return IntStream.range(0, 4)
+                .map(i -> {
+                    int matches = GridUtility.getPatternMatches(grid, pattern).size();
+                    GridUtility.rotateGridClockwise(pattern);
+                    return matches;
+                })
+                .sum();
     }
 
 }
